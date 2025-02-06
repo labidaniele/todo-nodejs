df8fc2adc50e8b8aa28bee9efff329dee14a549e
Sky <labidaniele@itis-molinari.eu>
2025-02-06 22:48:40 +0100

    db

--- db/index.js
+++ db/index.js
@@ -1,61 +1,26 @@
-const fs = require('fs');
-const mysql = require('mysql2');
-const conf = JSON.parse(fs.readFileSync('conf.json'));
-const connection = mysql.createConnection(conf);
-
-const executeQuery = (sql) => {
-    return new Promise((resolve, reject) => {      
-          connection.query(sql, function (err, result) {
-             if (err) {
-                console.error(err);
-                reject();     
-             }   
-             console.log('done');
-             resolve(result);         
-       });
-    })
- }
-
- const createTable = () => {
-    return executeQuery(`
-    CREATE TABLE IF NOT EXISTS todo
-       ( id INT PRIMARY KEY AUTO_INCREMENT, 
-          name VARCHAR(255) NOT NULL, 
-          completed BOOLEAN ) 
-       `);      
- }
-
- const insert = (todo) => {
-    const template = `
-    INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
-       `;
-    let sql = template.replace("$NAME", todo.name);
-    sql = sql.replace("$COMPLETED", todo.completed);
-    return executeQuery(sql); 
- }
-
- const select = () => {
-    const sql = `
-    SELECT id, name, completed FROM todo 
-       `;
-    return executeQuery(sql); 
- }
-
- [
-    { 
-         id: 1, 
-         name: 'studiare TPSI', 
-         completed: 0 
-  },
-  { 
-         id: 2, 
-         name: 'ripassare Storia', 
-         completed: 0 
-  }
-  ]
-
-  createTable().then(() => {
-    insert({name: "test " + new Date().getTime(), completed: false}).then((result) => {
-       select().then(console.log);
-    });
- });
+(async () => {
+   const inputFile = document.querySelector('#file');
+   const button = document.querySelector("#button");
+   const link = document.querySelector("#link");
+ 
+   handleSubmit = async (event) => {
+     const formData = new FormData();
+     formData.append("file", inputFile.files[0]);
+     const body = formData;
+     body.description = inputDescription.value;
+     const fetchOptions = {
+       method: 'post',
+       body: body
+     };
+     try {
+       const res = await fetch("/upload", fetchOptions);
+       const data = await res.json();
+       link.setAttribute("href", data.url);
+       link.innerText = data.url;
+     } catch (e) {
+       console.log(e);
+     }
+   }
+ 
+   button.onclick = handleSubmit;
+ })();
--- db/server.js
+++ db/server.js
@@ -1,50 +1,29 @@
 const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
-const http = require("http");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-   extended: true
-}));
+const http = require('http');
 const path = require('path');
-app.use("/", express.static(path.join(__dirname, "public")));
-
-let todos = [];
-
-app.post("/todo/add", (req, res) => {
-   const todo = req.body.todo;
-   todo.id = "" + new Date().getTime();
-   todos.push(todo);
-   res.json({result: "Ok"});
-});
-
-app.get("/todo", (req, res) => {
-   res.json({todos: todos});
-});
-
-app.put("/todo/complete", (req, res) => {
-    const todo = req.body;
-    try {
-       todos = todos.map((element) => {
-          if (element.id === todo.id) {
-             element.completed = true;
-          }
-          return element;
-       })
-    } catch (e) {
-       console.log(e);
+const app = express();
+const multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, "files"));
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
     }
-    res.json({result: "Ok"});
- });
+});
+const upload = multer({ storage: storage}).single('file');
 
-
- app.delete("/todo/:id", (req, res) => {
-    todos = todos.filter((element) => element.id !== req.params.id);
-    res.json({result: "Ok"});  
- })
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/files", express.static(path.join(__dirname, "files")));
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        
+    console.log(req.file.filename);    
+    res.json({url: "./files/" + req.file.filename});    
+  })
+});
 
 const server = http.createServer(app);
-
-server.listen(5500, () => {
+server.listen(5600, () => {
   console.log("- server running");
 });
